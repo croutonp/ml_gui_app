79740222be137341fa73f1118eff8a9f882321c6
crouton <cgpasaoa@gmail.com>
2024-04-23 11:29:19 -0400

    update

--- starterFiles/classifier.py
+++ starterFiles/classifier.py
@@ -1,4 +1,12 @@
 from taipy.gui import Gui
+from tensorflow.keras import models
+
+models.load_model("baseline_mariya.keras")
+
+def predict_image(model, path_to_img):
+    print(model.summary())
+    print(path_to_img)
+
 
 img_path = "placeholder_image.png"
 
@@ -6,18 +14,23 @@ content = ""
 
 index = """
 <|text-center|
-<|{"logo.png"}|image|>
+<|{"logo.png"}|image|width=25vw|>
 
-<|{content}|file_selector|>
+<|{content}|file_selector|extensions=.png|>
 select an image from your file system
 
 <|{img_path}|image|>
+
+<|label goes here|indicator|value=0|min=0|max=100|width=25vw|>
 
 >
 """
 
 def on_change(state, var_name, var_val):
-    print(var_name, var_val)
+    if var_name == "content":
+        state.img_path = var_val
+        predict_image(model, var_val)
+    # print(var_name, var_val)
 
 app = Gui(page=index)
 
